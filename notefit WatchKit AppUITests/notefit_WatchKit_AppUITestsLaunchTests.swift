6907a253ddce5afbf182d8401036425bd33dd7cd
pdipaolo <pdipaolo91@gmail.com>
2022-10-15 17:29:13 +0200

    Initial Commit

---
+++ notefit WatchKit AppUITests/notefit_WatchKit_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  notefit_WatchKit_AppUITestsLaunchTests.swift
+//  notefit WatchKit AppUITests
+//
+//  Created by Pierluigi Di paolo on 15/10/22.
+//
+
+import XCTest
+
+class notefit_WatchKit_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
