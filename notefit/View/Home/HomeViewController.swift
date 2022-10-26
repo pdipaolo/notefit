36ff355efd1f5305b21fb78749dbd30c226f99c7
pdipaolo <pdipaolo91@gmail.com>
2022-10-26 18:16:22 +0200

    add header and plan collectionview

--- notefit/View/Home/HomeViewController.swift
+++ notefit/View/Home/HomeViewController.swift
@@ -9,21 +9,41 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var homeCollectionView: UICollectionView!
+    let reuseIdentifier = ["PlanCell", "InfoCell"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        homeCollectionView.delegate = self
+        homeCollectionView.dataSource = self
+        homeCollectionView.register(UINib(nibName: "PlanCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "PlanCell")
+        homeCollectionView.register(UINib(nibName: "InfoCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "InfoCell")
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+extension HomeViewController: UICollectionViewDelegate {
+    
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier[0], for: indexPath)
+        return cell
+    }
+    
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 140)
+    }
+}
+
---
+++ notefit/View/Home/InfoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  InfoCollectionViewCell.swift
+//  notefit
+//
+//  Created by Pierluigi Di paolo on 26/10/22.
+//
+
+import UIKit
+
+class InfoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
